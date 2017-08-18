88e486e7efd1108360c67670c6d5baf662d23bb6
xiaofan <zeng255@163.com>
2017-08-18 20:40:19 +0800

    update for solr6.6.0

--- src/main/java/org/wltea/analyzer/sample/IKAnalyzerDemo.java
+++ src/main/java/org/wltea/analyzer/sample/IKAnalyzerDemo.java
@@ -1,96 +1,81 @@
 /**
- * 
+ * IK 中文分词  版本 5.0.1
+ * IK Analyzer release 5.0.1
+ * <p>
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * 源代码由林良益(linliangyi2005@gmail.com)提供
+ * 版权声明 2012，乌龙茶工作室
+ * provided by Linliangyi and copyright 2012 by Oolong studio
  */
 package org.wltea.analyzer.sample;
 
 import java.io.IOException;
+import java.io.StringReader;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.index.CorruptIndexException;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.LockObtainFailedException;
-import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
 import org.wltea.analyzer.lucene.IKAnalyzer;
-import org.wltea.analyzer.lucene.IKQueryParser;
-import org.wltea.analyzer.lucene.IKSimilarity;
 
 /**
- * @author linly
+ * 使用IKAnalyzer进行分词的演示 2012-10-22
  *
  */
 public class IKAnalyzerDemo {
-	
-	public static void main(String[] args){
-		//Lucene Document的域名
-		String fieldName = "text";
-		 //检索内容
-		String text = "IK Analyzer是一个结合词典分词和文法分词的中文分词开源工具包。它使用了全新的正向迭代最细粒度切分算法。";
-		
-		//实例化IKAnalyzer分词器
-		Analyzer analyzer = new IKAnalyzer();
-	 
-		
-		Directory directory = null;
-		IndexWriter iwriter = null;
-		IndexSearcher isearcher = null;
-		try {
-			//建立内存索引对象
-			directory = new RAMDirectory();	 
-			iwriter = new IndexWriter(directory, analyzer, true , IndexWriter.MaxFieldLength.LIMITED);
-			Document doc = new Document();
-			doc.add(new Field("ID", "10000", Field.Store.YES, Field.Index.NOT_ANALYZED));
-			doc.add(new Field(fieldName, text, Field.Store.YES, Field.Index.ANALYZED));
-			iwriter.addDocument(doc);
-			iwriter.close();
-			
-		    //实例化搜索器   
-			isearcher = new IndexSearcher(directory);			
-			//在索引器中使用IKSimilarity相似度评估器
-			isearcher.setSimilarity(new IKSimilarity());
-			
-			String keyword = "中文分词工具包";
-			
-			//使用IKQueryParser查询分析器构造Query对象
-			Query query = IKQueryParser.parse(fieldName, keyword);
-			
-			//搜索相似度最高的5条记录
-			TopDocs topDocs = isearcher.search(query , 5);
-			System.out.println("命中：" + topDocs.totalHits);
-			//输出结果
-			ScoreDoc[] scoreDocs = topDocs.scoreDocs;
-			for (int i = 0; i < topDocs.totalHits; i++){
-				Document targetDoc = isearcher.doc(scoreDocs[i].doc);
-				System.out.println("内容：" + targetDoc.toString());
-			}			
-			
-		} catch (CorruptIndexException e) {
-			e.printStackTrace();
-		} catch (LockObtainFailedException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally{
-			if(isearcher != null){
-				try {
-					isearcher.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			if(directory != null){
-				try {
-					directory.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+
+    public static void main(String[] args) {
+        // 构建IK分词器，使用smart分词模式
+        Analyzer analyzer = new IKAnalyzer(true);
+
+        // 获取Lucene的TokenStream对象
+        TokenStream ts = null;
+        try {
+            ts = analyzer.tokenStream("myfield", new StringReader("这是一个中文分词的例子，你可以直接运行它！IKAnalyer can analysis english text too"));
+            // 获取词元位置属性
+            OffsetAttribute offset = ts.addAttribute(OffsetAttribute.class);
+            // 获取词元文本属性
+            CharTermAttribute term = ts.addAttribute(CharTermAttribute.class);
+            // 获取词元文本属性
+            TypeAttribute type = ts.addAttribute(TypeAttribute.class);
+
+            // 重置TokenStream（重置StringReader）
+            ts.reset();
+            // 迭代获取分词结果
+            while (ts.incrementToken()) {
+                System.out.println(offset.startOffset() + " - " + offset.endOffset() + " : " + term.toString() + " | " + type.type());
+            }
+            // 关闭TokenStream（关闭StringReader）
+            ts.end(); // Perform end-of-stream operations, e.g. set the final offset.
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 释放TokenStream的所有资源
+            if (ts != null) {
+                try {
+                    ts.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
 }
